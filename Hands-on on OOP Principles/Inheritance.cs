3e76f1e8002cef7a2206a8bd274679a171eb710e
Sauravbasu10 <basukalasaurav@gmail.com>
2025-04-26 23:17:06 +0545

    OOP files added

---
+++ Hands-on on OOP Principles/Inheritance.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP;
+
+namespace OOP
+{
+    interface IAddition
+    {
+        double Add(double a, double b);
+
+    }
+
+    interface ISubtraction
+    {
+        double Subtract(double a, double b);
+
+    }
+
+    interface IMultiplication
+    {
+        double Multiply(double a, double b);
+
+    }
+
+    interface Idivision
+    {
+        double Divide(double a, double b);
+
+    }
+
+    internal class Inheritance : IAddition, ISubtraction, IMultiplication, Idivision
+    {
+        public double Add(double a, double b)
+        {
+            return (a + b);
+
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            if(b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Hands-on on OOP Principles/Overriding.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    // Base class
+    internal class BasicCalculator
+    {
+        public virtual int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public virtual int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public virtual int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public virtual double Divide(int a, int b)
+        {
+            if (b != 0)
+                return (double)a / b;
+            else
+                throw new DivideByZeroException("Cannot divide by zero.");
+        }
+    }
+
+    // Derived class overriding methods
+    internal class AdvancedCalculator : BasicCalculator
+    {
+        public override int Add(int a, int b)
+        {
+            Console.WriteLine("Using AdvancedCalculator Add:");
+            return base.Add(a, b);
+        }
+
+        public override int Subtract(int a, int b)
+        {
+            Console.WriteLine("Using AdvancedCalculator Subtract:");
+            return base.Subtract(a, b);
+        }
+
+        public override int Multiply(int a, int b)
+        {
+            Console.WriteLine("Using AdvancedCalculator Multiply:");
+            return base.Multiply(a, b);
+        }
+
+        public override double Divide(int a, int b)
+        {
+            Console.WriteLine("Using AdvancedCalculator Divide:");
+            return base.Divide(a, b);
+        }
+    }
+}
